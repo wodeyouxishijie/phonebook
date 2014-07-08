7a0455862a7f345c884ea17f4bb899ae30f7f4ec
yplgame <2875855980@qq.com>
2014-07-08 10:39:19 +0800

    'y'

---
+++ gitcommit.bat
@@ -0,0 +1,25 @@
+git add -A a
+::git add -A: [<path>]表示把<path>中所有tracked文件中被修改过或已删除文件和所有untracted的文件信息添加到索引库。
+::省略<path>表示.,即当前目录。
+
+git add -u
+::can deal with delete file
+::git add -u 表示 add to index only files modified or deleted and not those created 
+
+::git path
+::git add gitcommit.bat
+::git add b/3.txt
+
+git commit -m 'y'
+
+git push
+::要账号密码没解决
+
+
+::git clone https://github.com/yplgame/test.git
+::git status
+::git add -u
+::git commit -m 'test'
+::git push
+
+@cmd /k
---
+++ gitpull.bat
@@ -0,0 +1,3 @@
+::如果是ssh此无权限
+git pull
+@cmd /k
